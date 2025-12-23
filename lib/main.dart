d0189e410455393fb6e0a91d00db0f2b72418935
Purendra Jha <tpurendrajha@hlbb.hongleong.com.my>
2025-12-23 12:09:22 +0800

    Drawer Demo

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,69 @@
 import 'package:flutter/material.dart';
+import 'package:shopping_cart/pages/home_page.dart';
+import 'package:shopping_cart/pages/profile_page.dart';
+import 'package:shopping_cart/pages/settings_page.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
-      home: const Scaffold(),
+      debugShowCheckedModeBanner: false,
+      home: DrawerExample(),
+    );
+  }
+}
+
+class DrawerExample extends StatelessWidget {
+  DrawerExample({super.key});
+  final pages = [HomePage(), ProfilePage(), SettingsPage()];
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Drawer Example")),
+      drawer: Drawer(
+        backgroundColor: Colors.deepPurple[300],
+        child: ListView(
+          children: [
+            DrawerHeader(child: Icon(Icons.person)),
+            ListTile(
+              leading: Icon(Icons.home),
+              title: Text("Home"),
+              onTap: () {
+                Navigator.pop(context);
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => pages[0]),
+                );
+              },
+            ),
+
+            ListTile(
+              leading: Icon(Icons.favorite),
+              title: Text("Profile"),
+              onTap: () => Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => pages[1]),
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.settings),
+              title: Text("Settings"),
+              onTap: () => Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => pages[2]),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
