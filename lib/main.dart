827a481eb4e553cecf6e986ed0ed8ee64bfcbccb
Purendra Jha <tpurendrajha@hlbb.hongleong.com.my>
2025-12-30 22:49:43 +0800

    Remove authentication, user management, and associated UI components, updating main and home pages.

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:shopping_cart/pages/auth_page.dart';
 import 'package:shopping_cart/pages/home_page.dart';
-import 'package:shopping_cart/pages/profle_page.dart';
-import 'package:shopping_cart/pages/register_or_login_page.dart';
-import 'package:shopping_cart/pages/setting_page.dart';
-import 'package:shopping_cart/pages/users_page.dart';
 import 'package:shopping_cart/themes/theme_provider.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:shopping_cart/firebase_options.dart';
@@ -34,14 +29,8 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: Provider.of<ThemeProvider>(context).themeData,
-      home: AuthPage(),
-      routes: {
-        '/login_register_page': (context) => RegisterOrLoginPage(),
-        '/home_page': (context) => HomePage(),
-        '/profile_page': (context) => ProfilePage(),
-        '/setting_page': (context) => SettingPage(),
-        '/users_page': (context) => UsersPage(),
-      },
+      home: HomePage(),
+      routes: {'/home_page': (context) => HomePage()},
     );
   }
 }
